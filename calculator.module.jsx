bfe7fc141e9a448f2dd372bc127ac9e62793ef4d
rishabrajverma44 <113842786+rishabrajverma44@users.noreply.github.com>
2023-04-17 10:34:34 +0530

    Create calculator.module.jsx
    p1(CSS file)-Calculator is added

---
+++ calculator.module.jsx
@@ -0,0 +1,26 @@
+
+#main{
+  display: flex;
+  justify-content: center;
+}
+.sec{
+    border: 3px solid black;
+    border-radius: 20px;
+    width: 220px;
+    height: 320px;
+    margin-top: 150px;
+}
+.sec>input{
+    border-radius: 10px;
+    height: 30px;
+}
+.sec>button{
+    margin-left: 15px;
+    height: 30px;width: 35px;
+    border-radius: 10px;
+    font-weight: bolder;
+}
+.sec>label{
+    margin-left: 60px;
+    font-weight: bold;
+}
